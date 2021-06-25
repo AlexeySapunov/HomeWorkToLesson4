208a99de73e730047110f8a6f8864d372a131fd2
Alexey <blacka8607@rambler.ru>
2021-06-25 20:06:03 +0300

    Домашняя работа по уроку №4 "Продвинутые вопросы создания графического интерфейса"

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    public TextField textField;
+
+    public void btnSendClick(ActionEvent actionEvent) {
+        String text = textArea.getText() + textField.getText() + "\n";
+        textField.setText("");
+        textField.requestFocus();
+        textArea.setText(text);
+        textArea.setScrollTop(Double.MAX_VALUE);
+    }
+
+    public void menuExitSelect(ActionEvent actionEvent) {
+        System.out.println("Чат завершен");
+        System.exit(0);
+    }
 }
